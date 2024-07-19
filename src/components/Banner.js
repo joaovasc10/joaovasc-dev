6851685ec853e462f6207007eb273de2380295bc
João Vasconcelos <100876972+joaovasc10@users.noreply.github.com>
2024-07-19 20:53:14 -0300

    include images

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // images
-import Image from '../assets/avatar.svg';
+import Image from '../assets/autorretrato-removebg.png';
 // icons 
 import { FaGithub, FaYoutube, FaDribbble } from  'react-icons/fa'
 // type animation
@@ -13,6 +13,18 @@ import { fadeIn } from '../variants';
 const Banner = () => {
   return (
     <section className='section' id='home'>
+      <div className="container mx-auto">
+        <div>
+          {/* text */}
+          <div>
+            <h1>Web FullStack Developer</h1>
+          </div>
+          {/* image */}
+          <div>
+            <img src={ Image } alt='avatar' style={{ width: '482px', height: 'auto' }} />
+          </div>
+        </div>
+      </div>
       Home
     </section>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 //images
-import Logo from '../assets/logo.svg';
+import Logo from '../assets/logo-site.png';
+import LogoAlt from '../assets/logo.svg';
 
 const Header = () => {
   return (
@@ -10,7 +11,8 @@ const Header = () => {
         <div className='flex justify-between items-center '>
           {/* logo */}
           <a href='#'>
-            <img src={Logo} alt='logo'/>
+            <img src={ Logo } alt='logo' style={{ width:"135px", height:"75px" }}/>
+            {/* <img src={ LogoAlt } alt='logoAlt'/> */}
           </a>
           {/* button */}
           <button className='btn btn-sm'> Trabalhe comigo</button>
