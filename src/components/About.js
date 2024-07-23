6dde98b3b754be3d86f9b625bf3011bfb03e19b4
João Vasconcelos <100876972+joaovasc10@users.noreply.github.com>
2024-07-23 09:19:09 -0300

    created more sections

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,61 @@
 import React from 'react';
+// contup
+import CountUp from 'react-countup';
+// intersection observer hook
+import { useInView } from 'react-intersection-observer';
+// motion
+import { motion } from 'framer-motion';
+//variants
+import { fadeIn } from '../variants';
 
 const About = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
   return (
-    <div className='section' id='about'>
+    <section className='section' id='about' ref={ref}>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+          {/* image */}
+          <motion.div 
+            variants={ fadeIn('right', 0.3) }
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top'>
+          </motion.div>
+          {/* text */}
+          <motion.div 
+            variants={ fadeIn('left', 0.5) }
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{ once: false, amount: 0.3 }}
+            className='flex-1'>
+            <h2 className='h2 text-accent'>
+              Sobre mim
+            </h2>
+            <h3 className='h3 mb-4'>
+              Desenvolvedor web há 2 anos 
+            </h3>
+            <p className='mb-6'>lorem </p>
+            {/* stats */}
+            <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
+              <div>
+                <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                  {
+                    inView ? <CountUp start={0} end={2} duration={7} /> : null
+                  }
+                </div>
+                <div className='font-primary text-sm tracking-[2px]'>
+                  Anos de experiência
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        </div>
+      </div>
       Sobre
-    </div>
+    </section>
   );
 };
 
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 // images
 import Image from '../assets/autorretrato-removebg.png';
 // icons 
-import { FaGithub, FaYoutube, FaDribbble } from  'react-icons/fa'
+import { FaGithub, FaLinkedinIn, FaLinkedin } from  'react-icons/fa'
 // type animation
 import { TypeAnimation } from 'react-type-animation';
 // motion
@@ -12,20 +12,89 @@ import { fadeIn } from '../variants';
 
 const Banner = () => {
   return (
-    <section className='section' id='home'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className="container mx-auto">
-        <div>
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           {/* text */}
-          <div>
-            <h1>Web FullStack Developer</h1>
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <motion.h1 
+              variants={fadeIn('up', 0.3)} 
+              initial="hidden" 
+              whileInView={'show'} 
+              viewport={ {once: false, amount: 0.7 }}
+              className='text-[55px] font-bold leading-[0.8] lg:text-[35px]'>
+                Sou o joaovasc
+            </motion.h1>
+            <motion.div 
+              variants={fadeIn('up', 0.3)} 
+              initial="hidden" 
+              whileInView={'show'} 
+              viewport={ {once: false, amount: 0.7 }}
+              className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold leading-[1]'
+            >
+              <span className='text-white mr-4'>Web FullStack Developer</span>
+              <br/>
+
+              <TypeAnimation sequence={[
+                'e Fotógrafo 📷',
+                2000,
+                'e Videomaker 📹',
+                2000,
+                'e Ciclista 🚴',
+                2000,
+              ]}
+                speed={50}
+                className='text-accent'
+                wrapper='span'
+                repeat={Infinity}
+              />
+            </motion.div>
+            <motion.p 
+              variants={fadeIn('up', 0.3)} 
+              initial="hidden" 
+              whileInView={'show'} 
+              viewport={ {once: false, amount: 0.7 }}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'>
+                Desenvolvedor web há 2 anos.
+            </motion.p>
+            <motion.div 
+              variants={fadeIn('up', 0.3)} 
+              initial="hidden" 
+              whileInView={'show'} 
+              viewport={ {once: false, amount: 0.7 }}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+              <button className='btn btn-lg'>
+                Trabalhe comigo
+              </button>
+              <a href='/assets/Currículo-pt.pdf' download='Currículo-pt.pdf' className='text-gradient btn-link'>
+                Currículo
+              </a>
+            </motion.div>
+            {/* social */}
+            <motion.div 
+              variants={fadeIn('up', 0.7)} 
+              initial="hidden" 
+              whileInView={'show'} 
+              viewport={ {once: false, amount: 0.7 }}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+              <a href='https://github.com/joaovasc10' target="_blank" rel="noopener noreferrer">
+                <FaGithub size={24} className='text-white' />
+              </a>
+              <a href='https://www.linkedin.com/in/joaovasc10/' target="_blank" rel="noopener noreferrer">
+                <FaLinkedin size={24} className='text-white' />
+              </a>
+            </motion.div>
           </div>
           {/* image */}
-          <div>
-            <img src={ Image } alt='avatar' style={{ width: '482px', height: 'auto' }} />
-          </div>
+          <motion.div 
+            variants={fadeIn('up', 0.7)} 
+            initial="hidden" 
+            whileInView={'show'} 
+            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mx-auto mix-blend-lighten translate-y-500'>
+            <img src={ Image } alt='avatar'/>
+          </motion.div>
         </div>
       </div>
-      Home
     </section>
   );
 };
